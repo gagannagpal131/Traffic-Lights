e3a477181358fff8add2b8c138b9c81ccad9a852
Gagandeep Singh Nagpal <gagannagpal131@gmail.com>
2017-03-12 15:17:31 +0530

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Traffic Lights' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Traffic Lights
+	pod "AI" #, :path => "../"
+
+end
--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -7,12 +7,118 @@
 //
 
 import UIKit
+import AI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var textInput: UITextField!
+    
+    @IBOutlet weak var textOutput: UILabel!
+    
+    @IBOutlet var viewColor: UIView!
+    
+    
+    
+    fileprivate var response: QueryResponse? = .none
+  
+    
+    @IBAction func send(_ sender: Any) {
+        
+        
+        AI.sharedService.textRequest(textInput.text ?? "").success {(response) -> Void in
+            
+            self.response = response
+            DispatchQueue.main.async {
+                
+                print(response.result.fulfillment?.speech ?? "none")
+                
+                let output = response.result.fulfillment?.speech
+                
+                //print(response.result.parameters!)
+                //print(response)
+                print(response.result.metadata.intentName ?? "none")
+                //print(response.result.parameters?["colors"] ?? "none")
+                //print(response.result.action)
+                
+                if (response.result.metadata.intentName == "change.color"){
+                    
+                    
+                    
+                    let newColor = response.result.parameters?["colors"] as! String
+                    print(newColor)
+                    
+                    self.textOutput.text = output
+                    
+                    if( newColor == "red"){
+                        
+                        self.viewColor.backgroundColor = UIColor.red
+                    }else if(newColor == "green"){
+                        
+                        self.viewColor.backgroundColor = UIColor.green
+                        
+                    }else if(newColor == "yellow"){
+                        
+                        self.viewColor.backgroundColor  = UIColor.yellow
+                    }
+                    
+                    
+                }else{
+                    
+                    self.viewColor.backgroundColor = UIColor.white
+                    self.textOutput.text = output
+                    
+                }
+                
+                
+               
+                
+                
+            }
+            }.failure { (error) -> Void in
+                DispatchQueue.main.async {
+                    
+                    
+                    let alert = UIAlertController(title: "Please Enter Some Input", message: "Please try again", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    self.textOutput.text = ""
+                    
+        
+                    
+                    
+                }
+                
+                
+        }
+
+        
+        
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+         self.textInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
